8dfcc46e0faface398bf512e3d5947c0a6c72436
UnioviLCM27 <UO294237@uniovi.es>
2025-03-18 19:45:10 +0100

    Timed game interface created

---
+++ webapp/src/components/TimedGame.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import { Container, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import TimedQuestionPresentation from './wikidataComponents/QuestionPresentation.jsx';
+import QuestionGeneration from "./wikidataComponents/QuestionGeneration.js";
+
+
+const Game = () => {
+  const navigate = useNavigate();
+  const [currentQuestion, setCurrentQuestion] = useState(null);
+  const [questionGenerator] = useState(() => new QuestionGeneration(setCurrentQuestion));
+
+  useEffect(() => {
+    questionGenerator.fetchQuestions();
+  }, [questionGenerator]);
+
+  return (
+    <Container component="main" maxWidth="md" sx={{ marginTop: 4 }}>
+      <Typography component="h1" variant="h4">
+        Welcome to the Game Page
+      </Typography>
+      <TimedQuestionPresentation
+        timedGame={questionGenerator}
+        navigate={navigate}
+        question={currentQuestion}
+      />
+    </Container>
+  );
+};
+
+export default Game;
