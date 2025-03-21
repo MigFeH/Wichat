3fd03d62da2a5eac9586fe290658eb2422448f78
Miguel Fernández Huerta <miguelfdezhuerta@gmail.com>
2025-03-21 18:14:50 +0100

    Login and Register rework finished

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-//import Home from './components/Home';
 import Register from './components/Register';
 import Login from './components/Login';
 import Menu from './components/Menu';
--- webapp/src/components/Login.js
+++ webapp/src/components/Login.js
@@ -2,21 +2,16 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Container, Typography, TextField, Button, Snackbar, Link } from '@mui/material';
-//import { Typewriter } from "react-simple-typewriter";
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
   const [error, setError] = useState('');
-  const [loginSuccess, setLoginSuccess] = useState(false);
   const [createdAt, setCreatedAt] = useState('');
   const [openSnackbar, setOpenSnackbar] = useState(false);
 
   const apiEndpoint = process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000';
-  const apiKey = process.env.REACT_APP_LLM_API_KEY || 'None';
-
   const navigate = useNavigate();
 
   const loginUser = async () => {
@@ -32,20 +27,7 @@ const Login = () => {
 
       const response = await axios.post(`${apiEndpoint}/login`, { username, password });
 
-      const question = "Hey!! " + username + " who is going?";
-      const model = "empathy";
-
-      if (apiKey === 'None') {
-        setMessage("LLM API key is not set. Cannot contact the LLM.");
-      } else {
-        const message = await axios.post(`${apiEndpoint}/askllm`, { question, model, apiKey });
-        setMessage(message.data.answer);
-      }
-
       const { createdAt: userCreatedAt, token } = response.data;
-
-      setCreatedAt(userCreatedAt);
-      setLoginSuccess(true);
 
       if (token) {
         localStorage.setItem('authToken', token);
@@ -63,52 +45,6 @@ const Login = () => {
   const handleCloseSnackbar = () => {
     setOpenSnackbar(false);
   };
-
-  /*return (
-    <Container component="main" maxWidth="xs" sx={{ marginTop: 4 }}>
-      {loginSuccess ? (
-        <div>
-          <Typewriter
-            words={[message]}
-            cursor
-            cursorStyle="|"
-            typeSpeed={50}
-          />
-          <Typography component="p" variant="body1" sx={{ textAlign: 'center', marginTop: 2 }}>
-            Your account was created on {new Date(createdAt).toLocaleDateString()}.
-          </Typography>
-        </div>
-      ) : (
-        <div>
-          <Typography component="h1" variant="h5">
-            Login
-          </Typography>
-          <TextField
-            margin="normal"
-            fullWidth
-            label="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <TextField
-            margin="normal"
-            fullWidth
-            label="Password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Button variant="contained" color="primary" onClick={loginUser}>
-            Login
-          </Button>
-          <Snackbar open={openSnackbar} autoHideDuration={6000} onClose={handleCloseSnackbar} message="Login successful" />
-          {error && (
-            <Snackbar open={!!error} autoHideDuration={6000} onClose={() => setError('')} message={`Error: ${error}`} />
-          )}
-        </div>
-      )}
-    </Container>
-  );*/
 
   return (
     <Container component="main" maxWidth="xs" sx={{minHeight: "100vh", width: "100%", padding: 0 }}>
