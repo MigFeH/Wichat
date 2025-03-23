3bb98ab43543e055f026b7ac0b6087d69f785893
josemzuvi <uo294061@uniovi.es>
2025-03-23 19:47:18 +0100

    Mejorada la estetica del navbar

--- webapp/src/components/Navbar.js
+++ webapp/src/components/Navbar.js
@@ -1,17 +1,33 @@
 // src/components/Navbar.js
 import React from 'react';
-import { AppBar, Toolbar, Button } from '@mui/material';
+import { AppBar, Toolbar, Button, Box } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { Home, SportsEsports, BarChart, Leaderboard, ExitToApp } from '@mui/icons-material';
 
 const Navbar = () => {
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Button color="inherit" component={Link} to="/menu">Menu</Button>
-        <Button color="inherit" component={Link} to="/game">Game</Button>
-        <Button color="inherit" component={Link} to="/stadistics">Statistics</Button>
-        <Button color="inherit" component={Link} to="/ranking">Ranking</Button>
-        <Button color="inherit" component={Link} to="/login">Logout</Button>
+    <AppBar position="static" sx={{ backgroundColor: '#1976d2' }}>
+      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        {/* Contenedor de los botones de navegación */}
+        <Box sx={{ display: 'flex', gap: 2 }}>
+          <Button color="inherit" component={Link} to="/menu" startIcon={<Home />}>
+            Menu
+          </Button>
+          <Button color="inherit" component={Link} to="/game" startIcon={<SportsEsports />}>
+            Game
+          </Button>
+          <Button color="inherit" component={Link} to="/stadistics" startIcon={<BarChart />}>
+            Statistics
+          </Button>
+          <Button color="inherit" component={Link} to="/ranking" startIcon={<Leaderboard />}>
+            Ranking
+          </Button>
+        </Box>
+
+        {/* Botón de Logout alineado a la derecha */}
+        <Button color="inherit" component={Link} to="/login" startIcon={<ExitToApp />}>
+          Logout
+        </Button>
       </Toolbar>
     </AppBar>
   );
