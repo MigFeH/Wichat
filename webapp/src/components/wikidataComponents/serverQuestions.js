fe90c2353d9045c34f3d761cb86bc9c9c846282b
UnioviLCM27 <UO294237@uniovi.es>
2025-04-03 15:29:04 +0200

    test ampliation fixing - hopefully the last v2

--- webapp/src/components/wikidataComponents/serverQuestions.js
+++ webapp/src/components/wikidataComponents/serverQuestions.js
@@ -3,16 +3,15 @@ const cors = require('cors');
 const QuestionGeneration = require('./QuestionGeneration');
 
 const app = express();
-const port = 8004;
 app.disable('x-powered-by'); // Add this line to remove the version information
-
+const port = 8004;
 
 // Configure CORS with specific options
 const corsOptions = {
-  origin: process.env.CORS_ORIGIN || 'http://localhost:8004',
-  methods: ['GET'], // Allow only GET requests
-  allowedHeaders: ['Content-Type'],
-  maxAge: 600 // Cache preflight request results for 10 minutes (600 seconds)
+    origin: process.env.CORS_ORIGIN || 'http://localhost:3000', // Allow only your frontend origin
+    methods: ['GET'], // Allow only GET requests
+    allowedHeaders: ['Content-Type'],
+    maxAge: 600 // Cache preflight request results for 10 minutes (600 seconds)
 };
 
 app.use(cors(corsOptions));
@@ -30,7 +29,7 @@ app.get('/questions', async (req, res) => {
             return res.status(404).json({ error: "No questions available" });
         }
 
-        res.status(200).json(question);
+        res.json(question);
     } catch (error) {
         res.status(500).json({ error: "Error generating questions" });
     }
