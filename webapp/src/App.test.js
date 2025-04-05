e8e4944c14f134fd64a587bdcd2225f28017b67f
Ignacio Fernández Suárez <ignaciofsoviedo@gmail.com>
2025-04-05 17:03:09 +0200

    App test updated and passed ✅

--- webapp/src/App.test.js
+++ webapp/src/App.test.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
+import '@testing-library/jest-dom';
 
+// Mock components
 jest.mock('./components/Register', () => () => <div>Register Component</div>);
 jest.mock('./components/Login', () => () => <div>Login Component</div>);
 jest.mock('./components/Menu', () => () => <div>Menu Component</div>);
 jest.mock('./components/Game', () => () => <div>Game Component</div>);
+jest.mock('./components/TimedGame', () => () => <div>TimedGame Component</div>); // Added mock for TimedGame
 jest.mock('./components/Stadistics', () => () => <div>Stadistics Component</div>);
 jest.mock('./components/Ranking', () => () => <div>Ranking Component</div>);
-jest.mock('./auth/ProtectedRoute', () => ({ element }) => element);
+jest.mock('./components/Profile', () => () => <div>Profile Component</div>); // Added mock for Profile
+jest.mock('./auth/ProtectedRoute', () => ({ element }) => element); // Mock ProtectedRoute to simply render its element
 
 describe('App Routing', () => {
   it('renders Login Component for path "/"', () => {
@@ -57,6 +61,16 @@ describe('App Routing', () => {
     expect(screen.getByText('Game Component')).toBeInTheDocument();
   });
 
+  it('renders TimedGame Component for path "/timedGame"', () => {
+    render(
+      <MemoryRouter initialEntries={['/timedGame']} >
+        <App />
+      </MemoryRouter>
+    );
+    expect(screen.getByText('TimedGame Component')).toBeInTheDocument();
+  });
+
+
   it('renders Stadistics Component for path "/stadistics"', () => {
     render(
       <MemoryRouter initialEntries={['/stadistics']} >
@@ -74,4 +88,14 @@ describe('App Routing', () => {
     );
     expect(screen.getByText('Ranking Component')).toBeInTheDocument();
   });
+
+  it('renders Profile Component for path "/profile"', () => {
+    render(
+      <MemoryRouter initialEntries={['/profile']} >
+        <App />
+      </MemoryRouter>
+    );
+    expect(screen.getByText('Profile Component')).toBeInTheDocument();
+  });
+
 });
