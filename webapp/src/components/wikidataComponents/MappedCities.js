ebaf456a33509f5de4329a8af000cf2c8a1b3e7b
UnioviLCM27 <UO294237@uniovi.es>
2025-04-26 11:56:07 +0200

    trying to fix hospot, prometheus conf chng

--- webapp/src/components/wikidataComponents/MappedCities.js
+++ webapp/src/components/wikidataComponents/MappedCities.js
@@ -9,9 +9,6 @@ function getRandom() {
     const array = new Uint32Array(1);
     window.crypto.getRandomValues(array);
     return array[0] / (0xFFFFFFFF + 1);
-  } else {
-    // Fallback no seguro
-    return Math.random();
   }
 }
 
