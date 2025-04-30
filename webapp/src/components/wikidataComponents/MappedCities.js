c202c53587e637d70984adcd796d75259d6993db
Miguel Fernández Huerta <miguelfdezhuerta@gmail.com>
2025-04-30 21:56:39 +0200

    Security hotspot fixed

--- webapp/src/components/wikidataComponents/MappedCities.js
+++ webapp/src/components/wikidataComponents/MappedCities.js
@@ -3,12 +3,9 @@ import axios from "axios";
 let cityCache = []; // Lista de ciudades cargadas desde Wikidata
 
 function getRandom() {
-  if (typeof window !== 'undefined' && window.crypto && window.crypto.getRandomValues) {
     const array = new Uint32Array(1);
     window.crypto.getRandomValues(array);
     return array[0] / 0xFFFFFFFF;
-  }
-  return Math.random();
 }
 
 export async function fetchRandomCity() {
