b48de6bdcee10af1d5937a81a9402c23f8df750e
Miguel Fernández Huerta <miguelfdezhuerta@gmail.com>
2025-04-30 17:47:35 +0000

    Some changes to try to fix the Azure deploy

--- webapp/src/components/Login.js
+++ webapp/src/components/Login.js
@@ -10,7 +10,7 @@ const Login = () => {
   const [error, setError] = useState('');
   const [openSnackbar, setOpenSnackbar] = useState(false);
 
-  const apiEndpoint = process.env.GATEWAY_SERVICE_URL || 'http://localhost:8000';
+  const apiEndpoint = process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000';
   const navigate = useNavigate();
 
   const loginUser = async () => {
