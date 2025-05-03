399861bac06cfc86278bb4f999bd92aea58cb093
Miguel Fernández Huerta <miguelfdezhuerta@gmail.com>
2025-05-03 23:16:53 +0200

    Removing unused imports

--- webapp/src/components/utils/GameUtils.js
+++ webapp/src/components/utils/GameUtils.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, React } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import QuestionGeneration from "../wikidataComponents/QuestionGeneration";
 
 const useGameLogic = () => {
